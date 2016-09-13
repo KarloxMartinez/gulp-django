4d0e6deb981c74769ea38a135a95dfcfe495c9a7
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-09-13 18:59:30 -0500

    DO-60 Added wiredep package and improvents on scripts tasks

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,14 +3,22 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var notify = require('gulp-notify');
+var wiredep = require('wiredep')();
 
 gulp.task('scripts', function () {
     gulp.src(app.name + conf.path.js + '/**/*.js')
-        .pipe(concat('scripts.js'))
+        .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(app.name + conf.path.dist))
-        .pipe(browserSync.reload())
+        .pipe(gulp.dest(app.name + conf.path.dist)).on('change', browserSync.reload)
         .pipe(notify('scripts task done'));
+});
+
+gulp.task('scripts-vendor', function () {
+    gulp.src(wiredep.js)
+        .pipe(concat('component.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(app.name + conf.path.dist)).on('change', browserSync.reload)
+        .pipe(notify('scripts-vendor task done'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,11 +2,13 @@
 
 var gulp  = require('gulp');
 var browserSync = require('browser-sync');
+var wiredep = require('wiredep')();
 
 gulp.task('watch', [], function () {
     gulp.watch(app.name + conf.path.js + '/**/*.js', ['scripts']);
+    gulp.watch('bower.json', ['scripts-vendor']);
     gulp.watch(app.name + conf.path.sass + '/**/*.{sass,scss}', ['sass']);
-    gulp.watch([ 
-      app.name + conf.path.template + '/**/*.html', 
+    gulp.watch([
+      app.name + conf.path.template + '/**/*.html',
       app.name + conf.path.image + '/**/*.{svg,jpg,png,jpeg,gif}']).on('change', browserSync.reload);
 });
