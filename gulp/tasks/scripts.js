ad7089467b8ed3c93ac9c9c8da981e154c376909
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-09-15 09:18:23 -0500

    DO-60 Added ngAnnotate for uglify error on angular apps

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,9 +6,11 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var notify = require('gulp-notify');
 var bowerFiles    = require('bower-files')();
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('scripts', function () {
     gulp.src(app.name + conf.path.js + '/**/*.js')
+        .pipe(ngAnnotate())
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest(app.name + conf.path.dist)).on('change', browserSync.reload)
@@ -18,6 +20,7 @@ gulp.task('scripts', function () {
 gulp.task('scripts-vendor', function () {
     gulp.src(bowerFiles.ext('js').files)
         .pipe(concat('component.js'))
+        .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(gulp.dest(app.name + conf.path.dist)).on('change', browserSync.reload)
         .pipe(notify('scripts-vendor task done'));
