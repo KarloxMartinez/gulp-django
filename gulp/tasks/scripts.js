172ebd8f7d8cb76ab538270582babc68ec480ac3
Luis Carlos Martinez <karloxmartinez@hotmail.com>
2016-11-09 15:16:54 -0500

    The source maps are now in other file

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -11,22 +11,22 @@ var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function () {
     gulp.src(app.name + conf.path.js + '/**/*.js')
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init())
         .pipe(ngAnnotate())
         .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(app.name + conf.path.dist))
         .pipe(notify('scripts task done'));
 });
 
 gulp.task('scripts-vendor', function () {
     gulp.src(bowerFiles.ext('js').files)
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init())
         .pipe(concat('component.js'))
         .pipe(ngAnnotate())
         .pipe(uglify())
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(app.name + conf.path.dist))
         .pipe(notify('scripts-vendor task done'));
 });
